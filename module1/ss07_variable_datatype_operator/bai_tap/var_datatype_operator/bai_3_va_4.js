21d48fe57ace4a7099591148307edf2251276652
lethuandat <98041731+lethuandat@users.noreply.github.com>
2022-04-05 18:39:35 +0700

    Update bai_3_va_4.js

--- module1/ss07_variable_datatype_operator/bai_tap/var_datatype_operator/bai_3_va_4.js
+++ module1/ss07_variable_datatype_operator/bai_tap/var_datatype_operator/bai_3_va_4.js
@@ -1,5 +1,5 @@
 let r = prompt("Nhập bán kính hình tròn");
-let pi = 3.14;
+const pi = 3.14;
 document.write("Bán kính hình tròn, r = " + r);
 let c = 2 * r * pi;
 document.write("<br>Chu vi hình tròn, c = " + c);
